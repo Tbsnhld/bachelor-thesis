4fa937572707550594f43146bc5ce75b4c65edc8
Tobias <Tobias.neuhold@outlook.de>
2026-01-14 14:57:36 +0100

    added enums, removed unused imports

--- StatPriv/src/query.py
+++ StatPriv/src/query.py
@@ -1,8 +1,5 @@
 from abc import ABC, abstractmethod
-from scipy import stats
-from models.enums import DataSourceType
-from src.data_source import BernoulliSource
-import numpy as np
+from models.enums import QueryType
 
 class Query(ABC):
     @abstractmethod
@@ -34,7 +31,7 @@ class AverageQuery(Query):
         adversary_mean = (round((size - 1) * rv_mean) + int(database_conf.added_value)) / size
         adversary_variance = self.variance(database_conf, distribution_rv) 
 
-        var = database_conf.datasource.query_distribution(adversary_mean, adversary_variance)
+        var = database_conf.datasource.query_distribution(adversary_mean, QueryType.AVERAGE, adversary_variance)
         return var.pmf(observed_value) 
     
     def mean(self, database_conf, distribution_rv):
@@ -53,14 +50,25 @@ class AverageQuery(Query):
 
 class MedianQuery(Query):
     def execute(self, data):
-        return data[len(data)/2]
+        return data[int(round(len(data)/2))]
 
     def likelihood(self, database_conf, distribution_rv, observed_value):
-        median = distribution_rv.ppf(0.5)
+        mean = distribution_rv.mean()
         size = database_conf.size
-        adversary_median = ((size - 1) * median + int(database_conf.added_value)) / size 
-        clt_rv = database_conf.datasource.query_distribution(adversary_median)
-        return clt_rv.pdf(observed_value) 
+        adversary_mean = ((size - 1) * mean + int(database_conf.added_value)) / size 
+        adversary_variance = self.variance(database_conf, distribution_rv) 
+
+        var = database_conf.datasource.query_distribution(adversary_mean, adversary_variance)
+        return var.pdf(observed_value)
+
+    def discrete_likelihood(self, database_conf, distribution_rv, observed_value):
+        size = database_conf.size
+        rv_mean = distribution_rv.mean() 
+        adversary_mean = (round((size - 1) * rv_mean) + int(database_conf.added_value)) / size
+        adversary_variance = self.variance(database_conf, distribution_rv) 
+
+        var = database_conf.datasource.query_distribution(adversary_mean, QueryType.MEDIAN, adversary_variance)
+        return var.pmf(observed_value) 
 
     def mean(self, database_conf, distribution_rv):
         rv_mean = distribution_rv.mean()
@@ -72,7 +80,7 @@ class MedianQuery(Query):
         adversary_mean = self.mean(database_conf, distribution_rv)
         var = distribution_rv.var()
         original_contrib = (database_conf.size - 1) * (var + (rv_mean - adversary_mean)**2)
-        outlier_contrib = (database_conf.added_value - adversary_mean)**2
+        outlier_contrib = (float(database_conf.added_value) - adversary_mean)**2
         adversary_variance = (original_contrib + outlier_contrib) / database_conf.size
         return adversary_variance
     
