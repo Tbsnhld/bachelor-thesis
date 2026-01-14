02bad495c984ac8a11f74dee5226897e878b4766
Tobias <Tobias.neuhold@outlook.de>
2026-01-14 14:57:59 +0100

    added test for median

--- StatPriv/tests/test_datasource.py
+++ StatPriv/tests/test_datasource.py
@@ -18,9 +18,21 @@ class TestBernoulliSource:
         assert source.size == 10
         assert source.domain == [0, 1]
 
-    def test_likelihood_bernoulli(self, rng):
+    def test_likelihood_bernoulli_mean(self, rng):
         source = BernoulliSource(p=0.3, size=10)
         query = AverageQuery()
+        data = source.load_data(rng)
+         
+        database_conf = DatabaseConfig(seed=12345, query=query, datasource=source, added_value=1, size=10)
+        database_conf_2 = DatabaseConfig(seed=12345, query=query, datasource=source, added_value=0, size=10)
+        likelihood = source.likelihood(database_conf, query, observed_value=0.353)
+        likelihood2 = source.likelihood(database_conf_2, query, observed_value=0.353)
+
+        assert likelihood != likelihood2
+
+    def test_likelihood_bernoulli_median(self, rng):
+        source = BernoulliSource(p=0.45, size=10)
+        query = MedianQuery()
         data = source.load_data(rng)
          
         database_conf = DatabaseConfig(seed=12345, query=query, datasource=source, added_value=1, size=10)
@@ -50,7 +62,7 @@ class TestBernoulliSource:
         rv_2 = source.random_variable(probability=0.5, size=50)
 
         assert rv_1.mean()/50 == 0.3
-        assert rv_1.mean() == source.query_distribution(0.3).mean()
+        assert rv_1.mean() == source.query_distribution(0.3, "average").mean()
 
     def snap_observed(self):
         source = BernoulliSource(p=0.3, size=10)
