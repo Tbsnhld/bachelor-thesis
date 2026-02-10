b27f43c5efde7590640cff2b4c4923ad23286355
Tobias <Tobias.neuhold@outlook.de>
2026-02-10 19:22:10 +0100

    update enums

--- StatPriv/models/enums_mechanism.py
+++ StatPriv/models/enums_mechanism.py
@@ -2,10 +2,12 @@ from enum import Enum
 import src.mechanism as mechanisms
 
 class MechanismType(Enum):
-    GAUSSIAN = type(mechanisms.GaussianNoise) 
-    LAPLACE = type(mechanisms.LaplaceNoise) 
-    SUBSAMPLING = type(mechanisms.SubsamplingWithoutReplacement) 
-    SUBSAMPLING_REPLACEMENT = type(mechanisms.SubsamplingWithReplacement) 
-    POISSON_SUBSAMPLING = type(mechanisms.PoissonSubsampling) 
-    PURE_STATISTICAL_PRIVACY = type(mechanisms.PureStatisticalPrivacy)
+    GAUSSIAN =mechanisms.GaussianNoise 
+    GAUSSIAN_EPSILON = mechanisms.GaussianNoiseEpsilonDelta
+    LAPLACE = mechanisms.LaplaceNoise 
+    LAPLACE_EPSILON = mechanisms.GaussianNoiseEpsilonDelta
+    SUBSAMPLING = mechanisms.SubsamplingWithoutReplacement 
+    SUBSAMPLING_REPLACEMENT = mechanisms.SubsamplingWithReplacement
+    POISSON_SUBSAMPLING = mechanisms.PoissonSubsampling 
+    PURE_STATISTICAL_PRIVACY = mechanisms.PureStatisticalPrivacy
 
