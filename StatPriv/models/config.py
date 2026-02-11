bd6bdaec88d590de9e05213e3044c61d5a901213
Tobias <Tobias.neuhold@outlook.de>
2026-02-11 20:58:25 +0100

    update config

--- StatPriv/models/config.py
+++ StatPriv/models/config.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, replace
 from numpy._typing import _UnknownType
+from models.enums_query import QueryType
 from src.data_source import DataSource
 from src.query import Query
 from src.mechanism import Mechanism
@@ -12,9 +13,9 @@ class Config:
     query: Query | None
     added_values: _UnknownType | None
     mechanism: Mechanism | None
-    selected_database: int | None
     attack_type: str | None
     alpha: float | None
+    sensitivity: float | None
 
     def with_added_values(self, values) -> "Config":
         return replace(self, added_values=values) 
@@ -23,22 +24,38 @@ class Config:
         return replace(self, seed=seed) 
 
     def with_datasource(self, datasource) -> "Config":
-        return replace(self, datasource=datasource) 
+        sensitivity = self.calculate_sensitivity(self.query)
+        config = replace(self, datasource=datasource, sensitivity=sensitivity) 
+        return config
 
     def with_size(self, size) -> "Config":
-        return replace(self, size=size) 
+        sensitivity = self.calculate_sensitivity(self.query)
+        config = replace(self, size=size, sensitivity=sensitivity) 
+        return config
 
     def with_query(self, query) -> "Config":
-        return replace(self, query=query) 
+        sensitivity = self.calculate_sensitivity(query)
+        config = replace(self, query=query, sensitivity=sensitivity) 
+        return config
 
     def with_mechanism(self, mechanism) -> "Config":
         return replace(self, mechanism=mechanism) 
-
-    def with_selected_database(self, selected_database) -> "Config":
-        return replace(self, selected_database=selected_database) 
 
     def with_attack_model(self, attack_type) -> "Config":
         return replace(self, attack_type=attack_type)
 
     def with_alpha(self, alpha) -> "Config":
         return replace(self, alpha=alpha)
+
+    def calculate_sensitivity(self, query: Query):
+        if query != None and self.datasource != None and self.size != None:
+            if type(query) == QueryType.SUM.value :
+                sensitivity = self.datasource.max_diff
+            elif type(query) == QueryType.MEDIAN.value:
+                sensitivity = 1
+            else:
+                sensitivity = self.datasource.max_diff/self.size
+            return sensitivity
+        return None
+
+
